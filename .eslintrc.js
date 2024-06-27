70787ac3daac348c34a37efc321f671a81efc383
souheib-riache <souheib.riache@smartinia.com>
2024-06-27 14:24:20 +0200

    feat(app): project setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,7 +18,7 @@ module.exports = {
     ignorePatterns: ['.eslintrc.js'],
     rules: {    
       'no-unused-vars': 'warn',
-      '@typescript-eslint/no-unused-vars': 'error',
+      '@typescript-eslint/no-unused-vars': 'warn',
       '@typescript-eslint/interface-name-prefix': 'off',
       '@typescript-eslint/explicit-function-return-type': 'off',
       '@typescript-eslint/explicit-module-boundary-types': 'off',
