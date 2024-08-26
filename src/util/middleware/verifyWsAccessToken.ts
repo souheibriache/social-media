4cab924b44627daa71223f945420d709dae5ca08
souheibriache <gs_riache@esi.dz>
2024-08-26 15:36:06 +0200

    debug: log ws access toen guard

--- src/util/middleware/verifyWsAccessToken.ts
+++ src/util/middleware/verifyWsAccessToken.ts
@@ -4,7 +4,10 @@ import jwt from 'jsonwebtoken';
 const socketVerifyAccessToken = (socket: Socket, next: (err?: any) => void) => {
   try {
     const { ACCESS_TOKEN_PRIVATE_KEY } = process.env;
+    console.log({ ACCESS_TOKEN_PRIVATE_KEY });
     const token = socket.handshake.auth.token;
+
+    console.log({ token });
 
     if (!token) {
       return next(new Error('Unauthorized'));
@@ -15,6 +18,7 @@ const socketVerifyAccessToken = (socket: Socket, next: (err?: any) => void) => {
         return next(new Error('Unauthorized'));
       }
       socket.data.user = decoded;
+      console.log({ decoded });
       next();
     });
   } catch (err) {
